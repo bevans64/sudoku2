ffa7d1aeb5cefc2e370376b72688f2370a69889b
Brad Evans <brad.of.ottawa@gmail.com>
2018-07-23 21:34:39 -0400

    mode argument is display_matrix

--- display_matrix.py
+++ display_matrix.py
@@ -1,5 +1,10 @@
-def display_matrix():
+def display_matrix(mode):
    print "DEBUG: Displaying Suduku Board"
+   print "DEBUG: mode is (", mode,")"
+
+   # mode = 0 show only Sudoku board
+   #        1 also show board conflicts
+   #        2 also show square conflicts
 
    for x in range(sudMatrixDim):
       for y in range(sudMatrixDim):
@@ -10,7 +15,6 @@ def display_matrix():
          else:
             if NumState[x][y] == 1: # if fixed number
                print color.BOLD,
-            ## elif NumState[x][y] > 2: # if conflicting number
             elif NumState[x][y] == 3: # if conflicting number
                print color.RED,
             else:
@@ -22,41 +26,43 @@ def display_matrix():
          print "---------------------------------------",
       print
 
-   ## Display boardConfs for Debug
-   print ##
-   for x in range(sudMatrixDim): ##
-      for y in range(sudMatrixDim): ##
-         if y == 3 or y == 6: ##
-            print "|", ##
-         if boardConf[x][y] == 0: ##
-            print "   ", ##
-         else: ##
-            print color.CYAN, ##
-            print boardConf[x][y], ##
-            print color.END, # set colour normal ##
-      if x == 2 or x == 5: ##
-         print ##
-         print "---------------------------------------", ##
-      print ##
+   # Display boardConfs for Debug
+   if mode > 0:
+      print 
+      for x in range(sudMatrixDim): 
+         for y in range(sudMatrixDim): 
+            if y == 3 or y == 6: 
+               print "|", 
+            if boardConf[x][y] == 0: 
+               print "   ", 
+            else: 
+               print color.CYAN, 
+               print boardConf[x][y], 
+               print color.END, # set colour normal 
+         if x == 2 or x == 5: 
+            print 
+            print "---------------------------------------", 
+         print 
 
-   ## Display squareConfs for Debug
-   print ##
-   for x in range(sudMatrixDim): ##
-      for y in range(sudMatrixDim): ##
-         if y == 3 or y == 6: ##
-            print "|", ##
-         if squareConf[x][y] == 0: ##
-            print "   ", ##
-         else: ##
-            print color.GREEN, ##
-            print squareConf[x][y], ##
-            print color.END, # set colour normal ##
-      if x == 2 or x == 5: ##
-         print ##
-         print "---------------------------------------", ##
-      print ##
+   # Display squareConfs for Debug
+   if mode > 1:
+      print 
+      for x in range(sudMatrixDim): 
+         for y in range(sudMatrixDim): 
+            if y == 3 or y == 6: 
+               print "|", 
+            if squareConf[x][y] == 0: 
+               print "   ", 
+            else: 
+               print color.GREEN, 
+               print squareConf[x][y], 
+               print color.END, # set colour normal 
+         if x == 2 or x == 5: 
+            print 
+            print "---------------------------------------", 
+         print 
 
    return
 # End of function
 
-display_matrix()
+display_matrix(2)
