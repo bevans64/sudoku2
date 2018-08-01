8cdb5f94ea2a064fefff98a215326e790defaa09
Brad Evans <brad.of.ottawa@gmail.com>
2018-08-01 15:15:13 -0400

    map_number method (and sub methods) added

--- sudoku.py
+++ sudoku.py
@@ -13,10 +13,10 @@ class color:
    END = '\033[0m'
 
 class sudokuClass: 
-    _sudokuMatrix = [0] * 81 # Create Sudoku Matrix
+    _sudokuMatrix = [0] * 81 # Create Sudoku Matrix ## Change to _sudokuList
     _grid = [[0 for x in range(9)] for y in range(9)]
 
-    # NumState
+    # _numState
     # 0 - Unknown State (RED)
     # 1 - Fixed Number (WHITE)
     # 2 - Unconflicted Number (GREEN)
@@ -85,6 +85,53 @@ class sudokuClass:
 
     # end of display_matrix method
 
+    def map_number(self,number): ## Under construction
+       print("DEBUG: sudoku map_number method called. number is (", number, ")") ##
+
+       # Number must be between 1 and 9 inclusive
+       if number <1 or number > 9:
+          print("ERROR: map_number: number out of range")
+          return 1
+
+       for x in range(9):
+          for y in range(9):
+             if self._numState[x][y] == 1 or self._numState[x][y] == 2: # Skip if fixed number or unconflicted number exists
+                continue
+             elif self.test_fixed_num_in_square(number,x,y):
+                continue
+             elif self.test_board_conflict(number,x,y):
+                continue
+             else:
+                self._grid[x][y] = number
+    # end of map_number method
+
+    def test_board_conflict(self,number,x,y): # Does the number exist along the row or column ?
+       ## print("DEBUG: sudoku test_board_conflict method called. coordinates are are (", x,",",y, "). Number is (",number,")") ##
+
+       for z in range(9):
+          if number == self._grid[x][z] and self._numState[x][z] == 1: return True
+          if number == self._grid[z][y] and self._numState[z][y] == 1: return True
+       return False
+
+    # end of test_board_conflict method
+
+    def test_fixed_num_in_square(self,number,x,y): # Is number in same square?
+       ## print("DEBUG: sudoku test_fixed_num_insquare method called. coordinates are are (", x,",",y, "). Number is (",number,")") ##
+
+       # What Square are the coordinates in ?
+       a = x // 3
+       b = y // 3
+       square = (a) * 3 + (b)
+       ## print("DEBUG: square is ", square) ##
+       for j in range(a*3,a*3+3):
+          for k in range(b*3,b*3+3):
+             if number == self._grid[j][k] and self._numState[j][k] == 1:
+                ## print("DEBUG: ", number, "found in square", square)
+                return True
+
+       return False
+
+    # end of test_fixed_num_in_square method
 
 # End of sudokuClass
 
@@ -108,7 +155,9 @@ def main():
 
    ## sudoku._grid[0][0] = '4' ## test
    ## sudoku.reset_matrix() ##
+   sudoku.map_number(3)
+   ## sudoku.test_fixed_num_in_square(1,6,4)
    sudoku.display_matrix()
-   ## sudoku.export_matrix() ##
+   ##sudoku.export_matrix() ##
 
 if __name__ == '__main__': main()
